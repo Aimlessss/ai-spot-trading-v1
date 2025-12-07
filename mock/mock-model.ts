3d8e84e6d4476678a67d02df9593477b16847711
Atharva <katharva2002@gmail.com>
2025-12-07 14:19:10 +0530

    added random mock

--- mock/mock-model.ts
+++ mock/mock-model.ts
@@ -1,20 +1,20 @@
 export const mockmodel = async () => {
-  const decisions = [ "sell"] as const;
+  const decisions = [ "sell", "hold", "buy"] as const;
   const decision = decisions[Math.floor(Math.random() * decisions.length)];
 
   const quantity = String(Math.floor(Math.random() * 10) + 1);
 
-  // if (decision === "hold") {
-  //   return JSON.stringify({
-  //     decision: "hold",
-  //     reason: "Market conditions uncertain, waiting for confirmation.",
-  //     quantity,
-  //     next_check: {
-  //       target_price: (Math.random() * 90000 + 30000).toFixed(2),
-  //       wait_minutes: String(Math.floor(Math.random() * 5) + 1)
-  //     }
-  //   });
-  // }
+  if (decision === "hold") {
+    return JSON.stringify({
+      decision: "hold",
+      reason: "Market conditions uncertain, waiting for confirmation.",
+      quantity,
+      next_check: {
+        target_price: (Math.random() * 90000 + 30000).toFixed(2),
+        wait_minutes: String(Math.floor(Math.random() * 5) + 1)
+      }
+    });
+  }
 
   // buy or sell
   return JSON.stringify({
